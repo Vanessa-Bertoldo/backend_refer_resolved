2282e2ff72646c228858064973210aa670bc8237
Instaler Adm para instalação <vanessa.bert311@gmail.com>
2023-12-06 17:26:54 -0300

    insert filters

--- api/controller/nutritionController.js
+++ api/controller/nutritionController.js
@@ -5,10 +5,10 @@ const nutritionService = new NutritionService()
 class NutritionController{
     static async filterData(req, res){
         //const { dataInicial, dataFinal, classe, modo_pagamento } = req.body
-        const { matricula } = req.body
+        const { classe, dataInicial, dataFinal, modo_pagamento } = req.body
 
         try{
-            const data = await nutritionService.searchDataFilter(matricula)
+            const data = await nutritionService.searchDataFilter({ classe, dataInicial, dataFinal, modo_pagamento })
             res.status(200).json(data)
         }catch(error){
             res.status(400).send({ message: error.message })
--- api/services/nutritionService.js
+++ api/services/nutritionService.js
@@ -1,62 +1,32 @@
 const database = require("../models")
+const { Op } = require("sequelize");
 
 class NutritionService{
     
-    async searchDataFilter(matricula){
+    async searchDataFilter(dto){
         try{
-            /*const data = await database.TB_TICKET.findAll({
-                include: [
-                    {
-                        model: database.TB_FICHA,
-                        as: "fichas",
-                        attributes: ['matricula', 'nome', 'setor', 'classe', 'tamanho'],
-                        
-                    },
-                ],
-                where: {
-                    modo_pagamento: modo_pagamento
-                }
-            })*/
-           
             const data = await database.TB_FICHA.findAll({
                 include: [
                     {
                         model: database.TB_TICKET,
                         as: "tickets",
                         attributes: ['matricula', 'data', 'modo_pagamento', 'valor_pago', 'tamanho'],
-                        
+                        where: {
+                            data: {
+                                [Op.between]: [dto.dataInicial, dto.dataFinal]
+                            },
+                            modo_pagamento: dto.modo_pagamento
+                        }
                     },
                 ],
                 where: {
-                    matricula: matricula
+                    classe: dto.classe
                 }
             })
             return data
         } catch(error) {
-            throw new Error("Erro ao filtrar dados")
+            throw new Error("Erro ao filtrar dados ", error)
         }
     }
 }
 module.exports = NutritionService
-
-/**include: [
-                {
-                    model: database.roles,
-                    as: 'usuario_roles',
-                    attributes: ['id', 'nome', 'descricao'],
-                    through: {
-                        attributes: [],
-                    }
-                },
-                {
-                    model: database.permissoes,
-                    as: 'usuario_permissoes',
-                    attributes: ['id', 'nome', 'descricao'],
-                    through: {
-                        attributes: [],
-                    }
-                }
-            ],
-            where: {
-                id: dto.usuarioId
-            } */
